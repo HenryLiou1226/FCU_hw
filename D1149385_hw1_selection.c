c960150d0e1c31a2503c8565f0ac146d5b4d27cb
HenryLiou1226 <114418395+HenryLiou1226@users.noreply.github.com>
2022-11-27 19:55:26 +0800

    Update D1149385_hw1_selection.c

--- D1149385_hw1_selection.c
+++ D1149385_hw1_selection.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 int temp;
-int* selectionSort(int* array, int arrSize){
+int* selectionSort(int* array, int arrSize)
+{
     for (int step = 0;step < arrSize; step++)
+    {
+	for (int min_index = step+1; min_index < arrSize; min_index++)
 	{
-		for (int min_index = step+1; min_index < arrSize; min_index++)
+		if (array[min_index] < array[step])// Find the minimum element in unsorted array and swap the found minimum element with the first element
 		{
-			if (array[min_index] < array[step]){// Find the minimum element in unsorted array and swap the found minimum element with the first element
-				temp = array[min_index];
-				array[min_index] = array[step];
-				array[step] = temp;
-			}
+			temp = array[min_index];
+			array[min_index] = array[step];
+			array[step] = temp;
 		}
 	}
+    }
     return array;//The array is sorted, return to the main function
 }
 int main(void)
