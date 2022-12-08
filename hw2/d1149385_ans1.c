d126803b23cba05236682fe69768aece510af209
Henry <henry.liou2003@gmail.com>
2022-12-08 14:26:05 +0800

    12/8

---
+++ hw2/d1149385_ans1.c
@@ -0,0 +1,19 @@
+long long a[46];
+int t = 0;
+int climbStairs(int n){
+    if(t==0)
+    {
+        dd();
+        t+=1;
+    }
+    return a[n];
+}
+void dd()
+{
+    a[0]=1;
+    a[1]=1;
+    for(int i = 2;i < 46;i++)
+    {
+        a[i]=a[i-1]+a[i-2];
+    }
+}
